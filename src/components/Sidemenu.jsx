60b4d9a5e02e2fbdbaf9dff62e6ad34cb9158616
AjithAj10 <ajithraveendranr@gmil.com>
2024-01-14 11:55:21 +0530

    build failed

--- src/components/Sidemenu.jsx
+++ src/components/Sidemenu.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -25,7 +25,7 @@ const drawerWidth = 240;
 
 function sidemenu(props) {
   const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [mobileOpen, setMobileOpen] = useState(false);
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
